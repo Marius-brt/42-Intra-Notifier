9e1008f8cab6536b0503c21e5852c7c0f24eea0f
Marius Brouty <marius.brouty@gmail.com>
2021-09-10 00:03:06 +0200

    Dark theme support

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function spawnWindow(){
 		width: 800,
 		height: 600,
 		autoHideMenuBar: true,
+		frame: false,
+		show: false,
 		webPreferences: {
 			nativeWindowOpen: false
 		}
@@ -22,4 +24,7 @@ function spawnWindow(){
 	win.setMenu(null)
 	//win.loadFile('./app/index.html')
 	win.loadURL("http://127.0.0.1:5500/app/index.html")
+	win.once('ready-to-show', () => {
+		win.show()
+	})
 }
